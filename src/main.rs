12134acf5751d94d62be6bbec0eac9347bc4be5c
Erik Näslund <contact@eriknaslund.com>
2023-11-15 15:31:11 +0100

    Add project specific settings

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+// As a blog visitor,
+// I want to subscribe to the newsletter,
+// So that I can receive email updates when new posts are published.
+
+// As the blog author,
+// I want to send email updates to subscribers,
+// So that I can notify them when new posts are published.
