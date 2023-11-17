52313f5f854bbbff3d789ad2a2c34af8137a1360
Erik Näslund <contact@eriknaslund.com>
2023-11-17 16:31:59 +0100

    Fix app address bug

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
-    let address = format!("127:0.0.1:{}", configuration.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
